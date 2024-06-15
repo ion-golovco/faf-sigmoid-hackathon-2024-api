923084fc1eb660c3fbdfc44a46072260a0900571
L4urent1u <101964769+L4urent1u@users.noreply.github.com>
2024-06-15 16:05:29 +0300

    login and register (#2)

---
+++ routes/login.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const supabase = require('../supabase');
+
+// POST /login
+router.post('/', async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        // Attempt to sign in the user
+        const { data, error } = await supabase.auth.signIn({
+            email,
+            password,
+        });
+
+        if (error) {
+            res.status(401).json({ error })
+        }
+
+        // User successfully logged in
+        res.json({ token: data.session.access_token });
+    } catch (error) {
+        res.status(401).json({ error: error.message });
+    }
+});
+
+module.exports = router;
