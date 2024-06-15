b964673bd8834e0d2ceed9e6af3a4d07302198b7
L4urent1u <laurentiu.blidari2@gmail.com>
2024-06-16 00:04:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -57,26 +57,20 @@ app.post('/register', async (req, res, next) => {
 
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_API_KEY,
+  apiKey: "sk-proj-3vDaYkleGM3Mib3GnPh4T3BlbkFJ9vz9C5Bebx2EUHEeEXGp",
 });
 const openai = new OpenAIApi(configuration);
 
-const conversationContextPrompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ";
+const promt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ";
 
 app.post('/chat', async function (req, res, next) {  // Extracting the user's message from the request body 
   const message = req.body.message;
-  console.log(message)
   try {
-    openai.createCompletion({
+    openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      // Adding the conversation context to the message being sent 
-      prompt: conversationContextPrompt + message,
-      temperature: 0.9,
-      max_tokens: 150,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.6,
-      stop: [" Human:", " AI:"],
+      messages: [{
+        role: "user", content: `${promt} ${message}`
+      }]
     })
       .then((response) => {
         res.json(response.data.choices);
