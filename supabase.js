49d8595bde00e3d00d0bc72a8faf263d662f47a9
ion-golovco <golovcoion0401@gmail.com>
2024-08-01 10:44:11 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://jjrdkedwvpctmwkiboso.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqcmRrZWR3dnBjdG13a2lib3NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NDMwNzUsImV4cCI6MjAzNDAxOTA3NX0.wVQfele2bUs2Bb-MVswOgl-TLRPU1MKkcd_oKQ3N4EE';
+const supabaseUrl = process.env.SUPABASE_API_URL;
+const supabaseKey = process.env.SUPABASE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
